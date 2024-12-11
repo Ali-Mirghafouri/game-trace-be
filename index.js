2c4f8decd74cd96f9a0a34371070c319b4e249d1
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2024-12-11 17:44:01 +0400

    corse

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: "https://game-trace-be.onrender.com", // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
